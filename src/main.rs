a796de75a5dcb75b4681e59e206bef92c9b50017
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2022-11-03 17:17:12 +0100

    Added working code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,11 @@ use crate::tensornetwork::tensor::Tensor;
 use crate::tensornetwork::TensorNetwork;
 
 fn main() {
-    let mut t= TensorNetwork::new(
-        vec![
-            Tensor::new(vec![4, 3, 2]),
-            Tensor::new(vec![0, 1, 3, 2]),
-        ],
+    let tn = TensorNetwork::new(
+        vec![Tensor::new(vec![4, 3, 2]), Tensor::new(vec![0, 1, 3, 2])],
         vec![17, 18, 19, 12, 22],
     );
-    let bad_tensor = Tensor::new(vec![0, 1, 4]);
-    let bad_bond_dims = vec![12, 32, 2];
-    t.push_tensor(bad_tensor, Some(bad_bond_dims));
-
-    println!("My Tensornetwork\n{}!", t);
+    for edge in tn.get_edges(){
+        println!("{:}->({:}, {:})", edge.0, (*edge.1).0.unwrap_or_else(|| -1) , (*edge.1).1.unwrap_or_else(|| -1));
+    }
 }
