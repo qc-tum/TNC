3d73d99591632ca187350589626bfa55e9412b73
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2022-11-23 16:51:36 +0100

    Updating basic-usage and benchmark to use new TensorNetwork interface

--- src/bin/basic-usage.rs
+++ src/bin/basic-usage.rs
@@ -1,11 +1,16 @@
+
+extern crate tensorcontraction;
 use tensorcontraction::tensornetwork::{TensorNetwork, tensor::Tensor};
+use tensorcontraction::tensornetwork::contractionpath::{contract_cost, contract_size};
 
 fn main() {
-    let tn = TensorNetwork::new(
+    let tn = TensorNetwork::from_vector(
         vec![Tensor::new(vec![4, 3, 2]), Tensor::new(vec![0, 1, 3, 2])],
         vec![17, 18, 19, 12, 22],
     );
     for edge in tn.get_edges(){
         println!("{:}->({:}, {:})", edge.0, (*edge.1).0.unwrap_or_else(|| -1) , (*edge.1).1.unwrap_or_else(|| -1));
     }
+    println!("{}", contract_cost(&tn, 0, 1));
+    println!("{}", contract_size(&tn, 0, 1));
 }
