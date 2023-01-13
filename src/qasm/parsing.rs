02b9b7e1f5973a3d906e8b4702fcb56652707956
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2023-01-13 10:57:40 +0100

    Implement qasm include fill in
    Does only work for includes in the root file, i.e. does not work
    recursively at the moment.
    
    The problem is that the included files are not valid QASM files
    themselves. Hence they can't be parsed on their own (to find includes),
    but they have to be first inserted into the main code, which can then be
    parsed from start to end again. All includes would then be relative to
    the main file though, and not to the included file.
    
    A solution would be to store for the assembled code the source of each
    block of text, such that when a new include is resolved, it can be done
    relative to the file that the include was originally from.

--- src/qasm/parsing.rs
+++ src/qasm/parsing.rs
@@ -1,41 +1,123 @@
+use std::collections::HashSet;
+use std::fs;
+use std::path::PathBuf;
+
+use antlr_rust::common_token_stream::CommonTokenStream;
 use antlr_rust::tree::{ParseTree, ParseTreeVisitorCompat};
+use antlr_rust::InputStream;
 
-use super::qasm2parser::VersionContext;
-use super::{
-    qasm2parser::{Qasm2ParserContextType, VersionContextAttrs},
-    qasm2parservisitor::Qasm2ParserVisitorCompat,
-};
+use super::qasm2lexer::Qasm2Lexer;
+use super::qasm2parser::Qasm2ParserContextType;
+use super::qasm2parser::{IncludeStatementContextAttrs, Qasm2Parser};
+use super::qasm2parservisitor::Qasm2ParserVisitorCompat;
+use antlr_rust::tree::Visitable;
 
-pub struct MyVisitor {
-    result: Option<f32>,
+static QELIB: &'static str = include_str!("qelib1.inc");
+
+/// Holds the position and content of an include statement in a string.
+#[derive(Clone, Debug)]
+pub struct IncludeInstruction {
+    path: String,
+    start: usize,
+    end: usize,
 }
 
-impl Default for MyVisitor {
+/// Visitor collecting all include statements in a file.
+pub struct IncludeVisitor {
+    result: Vec<IncludeInstruction>,
+}
+
+impl Default for IncludeVisitor {
     fn default() -> Self {
-        Self { result: None }
+        Self { result: Vec::new() }
     }
 }
 
-impl ParseTreeVisitorCompat<'_> for MyVisitor {
+impl ParseTreeVisitorCompat<'_> for IncludeVisitor {
     type Node = Qasm2ParserContextType;
-    type Return = Option<f32>;
+    type Return = Vec<IncludeInstruction>;
 
     fn temp_result(&mut self) -> &mut Self::Return {
         &mut self.result
     }
 
     fn aggregate_results(&self, aggregate: Self::Return, next: Self::Return) -> Self::Return {
-        aggregate.or(next)
+        let mut res = aggregate.clone();
+        res.extend_from_slice(&next);
+        res
     }
 }
 
-impl Qasm2ParserVisitorCompat<'_> for MyVisitor {
-    fn visit_version(&mut self, ctx: &VersionContext) -> Self::Return {
-        Some(ctx.VersionSpecifier()
-            .unwrap()
-            .get_text()
-            .parse()
-            .unwrap())
+impl Qasm2ParserVisitorCompat<'_> for IncludeVisitor {
+    fn visit_includeStatement(
+        &mut self,
+        ctx: &super::qasm2parser::IncludeStatementContext,
+    ) -> Self::Return {
+        let start = ctx.INCLUDE().unwrap().symbol.start as usize;
+        let end = ctx.SEMICOLON().unwrap().symbol.stop as usize;
+        let include_path = ctx.StringLiteral().unwrap().get_text();
+        let include_path = &include_path[1..include_path.len() - 1];
+        vec![IncludeInstruction {
+            path: String::from(include_path),
+            start,
+            end,
+        }]
+    }
+}
+
+/// Parses the QASM2 code and returns a list of all include instructions in the order
+/// they were found.
+fn parse_includes(code: &str) -> Vec<IncludeInstruction> {
+    let lexer = Qasm2Lexer::new(InputStream::new(code));
+    let token_source = CommonTokenStream::new(lexer);
+    let mut parser = Qasm2Parser::new(token_source);
+    let parsed = parser.program().unwrap();
+
+    let mut visitor = IncludeVisitor::default();
+    parsed.accept(&mut visitor);
+    visitor.result
+}
+
+/// Parses the file for include statements, loads the corresponding files and
+/// replaces the statements by the file contents. Includes of the standard library
+/// "qelib1.inc" will directly be replaced by the corresponding code. This does only
+/// resolve includes in the original file, not in the included files.
+pub fn expand_includes(code: &mut String) {
+    let mut already_included = HashSet::new();
+    let includes = parse_includes(&code);
+
+    for include in includes.iter().rev() {
+        let is_stdlib = include.path == "qelib1.inc";
+
+        let path = if is_stdlib {
+            PathBuf::from(&include.path)
+        } else {
+            // Canonicalize paths to external files, such that relative and absolute
+            // paths to the same file are not seen as different paths
+            PathBuf::from(&include.path).canonicalize().unwrap()
+        };
+
+        // Check if the file was already included
+        if already_included.contains(&path) {
+            // We can safely ignore the include: the file was already included
+            // earlier, hence all definitions are already available
+            continue;
+        }
+
+        // Get code to include
+        let file_content;
+        let included_text = if is_stdlib {
+            // Use builtin qelib code
+            QELIB
+        } else {
+            // Load from external file
+            file_content = fs::read_to_string(&path).unwrap();
+            &file_content
+        };
+
+        // Include code in the root code
+        code.replace_range(include.start..=include.end, included_text);
+        already_included.insert(path);
     }
 }
 
@@ -44,23 +126,12 @@ mod tests {
 
     use std::fs;
 
-    use antlr_rust::{common_token_stream::CommonTokenStream, InputStream};
-
-    use antlr_rust::tree::Visitable;
-
-    use crate::qasm::{qasm2lexer::Qasm2Lexer, qasm2parser::Qasm2Parser};
-
-    use super::MyVisitor;
+    use crate::qasm::parsing::expand_includes;
 
     #[test]
     fn it_works() {
-        let input = fs::read_to_string("test.qasm").unwrap();
-        let lexer = Qasm2Lexer::new(InputStream::new(input.as_str()));
-        let token_source = CommonTokenStream::new(lexer);
-        let mut parser = Qasm2Parser::new(token_source);
-        let result = parser.program().unwrap();
-        let mut visitor = MyVisitor::default();
-        result.accept(&mut visitor);
-        println!("Visitor result: {:?}", visitor.result);
+        let mut code = fs::read_to_string("test.qasm").unwrap();
+        expand_includes(&mut code);
+        println!("{code}");
     }
 }
