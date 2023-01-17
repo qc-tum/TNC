c8f84f3baa830328178e45722367534502f6aecb
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-01-17 14:11:12 +0100

    Update  Branchbound
    Fix ssa_ordering to correctly update.
    Previous iteration could incorrectly convert in edge cases.
    Updated tests to ensure correctness
    Separate ssa and ssa with replace as both formats are useful

--- src/contractionpath/paths.rs
+++ src/contractionpath/paths.rs
@@ -7,18 +7,23 @@ use crate::contractionpath::contraction_cost::{_contract_cost, _contract_size, s
 use crate::tensornetwork::tensor::Tensor;
 use crate::tensornetwork::TensorNetwork;
 
-trait OptimizePath {
+pub trait OptimizePath {
     fn _optimize_path(&mut self, output: Option<Vec<u32>>);
+
+    fn get_best_path(self) -> Vec<(usize, usize)>;
+    fn get_best_replace_path(self) -> Vec<(usize, usize)>;
+    fn get_best_flops(self) -> u64;
+    fn get_best_size(self) -> u64;
 }
 
-enum BranchBoundType {
+pub enum BranchBoundType {
     Flops = 0,
     Size = 1,
 }
 
-enum CostFunction {}
-
-struct BranchBound {
+/// A struct that searches for an optimal contractionpath  given a [TensorNetwork] object and a list of
+/// TacoTensor objects containing tensor data.
+pub struct BranchBound {
     tn: TensorNetwork,
     nbranch: Option<u32>,
     cutoff_flops_factor: u64,
@@ -26,6 +31,7 @@ struct BranchBound {
     best_flops: u64,
     best_size: u64,
     best_path: Vec<(usize, usize)>,
+    // best_path: Vec<(usize, usize)>,
     best_progress: HashMap<usize, u64>,
     result_cache: HashMap<Vec<usize>, usize>,
     flop_cache: HashMap<usize, u64>,
@@ -33,12 +39,68 @@ struct BranchBound {
     tensor_cache: HashMap<usize, Tensor>,
 }
 
-/// Explores possible pair contractions in a depth-first recursive manner like
-/// the `optimal` approach, but with extra heuristic early pruning of branches
-/// as well sieving by `memory_limit` and the best path found so far. **Returns:**
-/// the lowest cost path. This algorithm still scales factorially with respect
-/// to the elements in the list `input_sets` if `nbranch` is not set, but it
-/// scales exponentially like `nbranch**len(input_sets)` otherwise.
+/// The contraction ordering labels [Tensor] objects from each possible contraction with a
+/// unique identifier in ssa format. As only a subset of these [Tensor] objects are seen in
+/// a contraction path, the tensors in the optimal path search are not sequential. This converts
+/// the output to strictly obey an ssa format.
+///
+/// # Arguments
+///
+/// * `path` - Output path as Vec<(usize, usize)> after an [_optimize_path] call.
+/// # Returns
+///
+/// Identical path using ssa format
+fn ssa_ordering(path: &Vec<(usize, usize, usize)>, mut n: usize) -> Vec<(usize, usize)> {
+    let mut ssa_path: Vec<(usize, usize)> = Vec::new();
+    let mut hs: HashMap<usize, usize> = HashMap::new();
+    let path_len = n;
+    for (u1, u2, u3) in path {
+        let t1: usize;
+        let t2: usize;
+        if *u1 > path_len {
+            t1 = hs[u1];
+        } else {
+            t1 = *u1;
+        }
+        if *u2 > path_len {
+            t2 = hs[u2];
+        } else {
+            t2 = *u2;
+        }
+        hs.entry(*u3).or_insert(n);
+        n += 1;
+        ssa_path.push((t1, t2));
+    }
+    ssa_path
+}
+
+/// Accepts a contraction path that is in ssa format and returns a contraction path
+/// assuming that all contracted tensors replace the left input tensor.
+///
+/// # Arguments
+///
+/// * `path` - Output path as Vec<(usize, usize)> that is in ssa format.
+/// # Returns
+///
+/// Identical path that replaces the left input tensor upon contraction
+fn ssa_replace_ordering(path: &Vec<(usize, usize)>, mut n: usize) -> Vec<(usize, usize)> {
+    let mut ssa_path = Vec::new();
+    let mut hs: HashMap<usize, usize> = HashMap::new();
+    let path_length = path.len();
+    for i in 0..path_length {
+        let mut tup = path[i];
+        if hs.contains_key(&tup.0) {
+            tup.0 = hs[&tup.0];
+        }
+        if hs.contains_key(&tup.1) {
+            tup.1 = hs[&tup.1];
+        }
+        hs.entry(n).or_insert(tup.0);
+        ssa_path.push(tup);
+        n += 1;
+    }
+    ssa_path
+}
 
 impl BranchBound {
     pub fn new(
@@ -63,9 +125,13 @@ impl BranchBound {
         }
     }
 
+    ///Explores possible pair contractions in a depth-first
+    /// recursive manner like the `optimal` approach, but with extra heuristic early pruning of branches
+    /// as well sieving by `memory_limit` and the best path found so far. A rust implementation of
+    /// the Python based `opt_einsum` implementation. Found at github.com/dgasmith/opt_einsum.
     fn _branch_iterate(
         &mut self,
-        path: Vec<(usize, usize)>,
+        path: Vec<(usize, usize, usize)>,
         remaining: Vec<u32>,
         mut flops: u64,
         mut size: u64,
@@ -73,7 +139,7 @@ impl BranchBound {
         if remaining.len() == 1 {
             self.best_size = size;
             self.best_flops = flops;
-            self.best_path = path.clone();
+            self.best_path = ssa_ordering(&path, self.tn.get_tensors().len());
         }
 
         let mut assess_candidate =
@@ -140,7 +206,7 @@ impl BranchBound {
 
         let bi = 0;
         let mut new_remaining;
-        let mut new_path: Vec<(usize, usize)>;
+        let mut new_path: Vec<(usize, usize, usize)>;
         while self.nbranch.is_none() || bi < self.nbranch.unwrap() {
             if candidates.is_empty() {
                 break;
@@ -152,15 +218,19 @@ impl BranchBound {
             new_remaining.retain(|e| *e != j as u32);
             new_remaining.insert(new_remaining.len(), k12 as u32);
             new_path = path.clone();
-            new_path.push((i, j));
+            new_path.push((i, j, k12));
             BranchBound::_branch_iterate(self, new_path, new_remaining, new_flops, new_size);
         }
     }
 }
 
+
 impl OptimizePath for BranchBound {
     fn _optimize_path(&mut self, _output: Option<Vec<u32>>) {
         let tensors = self.tn.get_tensors();
+        if self.tn.is_empty() {
+            return;
+        }
 
         self.flop_cache.clear();
         self.size_cache.clear();
@@ -178,12 +248,30 @@ impl OptimizePath for BranchBound {
         let remaining: Vec<u32> = (0u32..self.tn.get_tensors().len() as u32).collect();
         BranchBound::_branch_iterate(self, vec![], remaining, 0, 0);
     }
+
+    fn get_best_flops(self) -> u64 {
+        self.best_flops
+    }
+
+    fn get_best_size(self) -> u64 {
+        self.best_size
+    }
+
+    fn get_best_path(self) -> Vec<(usize, usize)> {
+        self.best_path
+    }
+
+    fn get_best_replace_path(self) -> Vec<(usize, usize)> {
+        ssa_replace_ordering(&self.best_path, self.tn.get_tensors().len())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     // use rand::distributions::{Distribution, Uniform};
     // TODO: Use random tensors
+    use super::ssa_ordering;
+    use super::ssa_replace_ordering;
     use crate::contractionpath::paths::BranchBound;
     use crate::contractionpath::paths::BranchBoundType;
     use crate::contractionpath::paths::OptimizePath;
@@ -216,13 +304,44 @@ mod tests {
     }
 
     #[test]
+    fn test_ssa_ordering() {
+        let path = vec![
+            (0, 3, 15),
+            (1, 2, 44),
+            (6, 4, 8),
+            (5, 15, 22),
+            (8, 44, 12),
+            (12, 22, 99),
+        ];
+        let new_path = ssa_ordering(&path, 7);
+
+        assert_eq!(
+            new_path,
+            vec![(0, 3), (1, 2), (6, 4), (5, 7), (9, 8), (11, 10)]
+        )
+    }
+
+    #[test]
+    fn test_ssa_replace_ordering() {
+        let path = vec![(0, 3), (1, 2), (6, 4), (5, 7), (9, 8), (11, 10)];
+        let new_path = ssa_replace_ordering(&path, 7);
+
+        assert_eq!(
+            new_path,
+            vec![(0, 3), (1, 2), (6, 4), (5, 0), (6, 1), (6, 5)]
+        )
+    }
+
+    #[test]
     fn test_contract_order_simple() {
         let tn = setup_simple();
         let mut opt = BranchBound::new(tn, None, 20, BranchBoundType::Flops);
         opt._optimize_path(None);
-        assert_eq!(opt.best_path, vec![(0, 1), (2, 3)]);
+
         assert_eq!(opt.best_flops, 568620);
         assert_eq!(opt.best_size, 90810);
+        assert_eq!(opt.best_path, vec![(0, 1), (2, 3)]);
+        assert_eq!(opt.get_best_replace_path(), vec![(0, 1), (2, 0)]);
     }
 
     #[test]
@@ -230,11 +349,10 @@ mod tests {
         let tn = setup_complex();
         let mut opt = BranchBound::new(tn, None, 20, BranchBoundType::Flops);
         opt._optimize_path(None);
-        assert_eq!(
-            opt.best_path,
-            vec![(0, 1), (2, 5), (3, 4), (6, 17), (18, 304)]
-        );
+        
         assert_eq!(opt.best_flops, 5614200);
         assert_eq!(opt.best_size, 3963645);
+        assert_eq!(opt.best_path, vec![(0, 1), (2, 5), (3, 4), (6, 7), (8, 9)]);
+        assert_eq!(opt.get_best_replace_path(), vec![(0, 1), (2, 5), (3, 4), (0, 2), (3, 0)]);
     }
 }
