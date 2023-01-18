1707952031f08d5b9bf61495c63c4df6e9db7082
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-01-18 17:26:47 +0100

    Update basic-usage to use right contract path

--- src/bin/basic-usage.rs
+++ src/bin/basic-usage.rs
@@ -15,7 +15,7 @@ fn main() {
     }
     let mut opt = BranchBound::new(r_tn.clone(), None, 20, BranchBoundType::Flops);
     opt._optimize_path(None);
-    let opt_path = opt.get_best_path();
+    let opt_path = opt.get_best_replace_path();
     println!("{:?}", opt_path);
 
     tn_contract(r_tn.clone(), d_tn, &opt_path);
