eb2d169b4722910cf3ce7a8f99793eed6e128e05
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2023-01-19 18:38:07 +0100

    Add larger test case for expression folder

--- src/qasm/expression_folder.rs
+++ src/qasm/expression_folder.rs
@@ -3,6 +3,7 @@ use super::{
     expression_simplification::simplify,
 };
 
+#[derive(Debug, Default)]
 struct ExpressionFolder;
 
 impl Visitor for ExpressionFolder {
@@ -30,7 +31,11 @@ impl Visitor for ExpressionFolder {
                     }
                 }
             }
-            Statement::GateCall { name, args, qargs } => {
+            Statement::GateCall {
+                name: _,
+                args,
+                qargs: _,
+            } => {
                 for expr in args.iter_mut() {
                     self.visit_expression(expr);
                 }
@@ -42,7 +47,7 @@ impl Visitor for ExpressionFolder {
 
 #[cfg(test)]
 mod tests {
-    use crate::qasm::ast::{BinOp, Expr, Statement, UnOp, Visitor};
+    use crate::qasm::ast::{BinOp, Expr, FuncType, Program, Statement, UnOp, Visitor};
 
     use super::ExpressionFolder;
 
@@ -73,14 +78,80 @@ mod tests {
             qargs: Vec::new(),
         };
 
-        let mut visitor = ExpressionFolder {};
+        let mut visitor = ExpressionFolder::default();
         visitor.visit_statement(&mut gc);
 
         // Check modified AST
-        if let Statement::GateCall { name, args, qargs } = gc {
+        if let Statement::GateCall {
+            name: _,
+            args,
+            qargs: _,
+        } = gc
+        {
             assert_eq!(args.len(), 2);
             assert_eq!(args[0], Expr::Float((2 + 3) as f64 * 4.0));
             assert_eq!(args[1], Expr::Int(-2 + 2));
+        } else {
+            panic!("Expected a gate call");
+        }
+    }
+
+    #[test]
+    fn simplify_program() {
+        let mut program = Program {
+            statements: vec![
+                Statement::GateCall {
+                    name: String::from("a"),
+                    args: vec![
+                        Expr::Binary(
+                            BinOp::Mul,
+                            Box::new(Expr::Int(3)),
+                            Box::new(Expr::Float(0.5)),
+                        ),
+                        Expr::Binary(
+                            BinOp::Div,
+                            Box::new(Expr::Float(3.6)),
+                            Box::new(Expr::Float(-2.4)),
+                        ),
+                    ],
+                    qargs: Vec::new(),
+                },
+                Statement::GateCall {
+                    name: String::from("b"),
+                    args: vec![Expr::Function(FuncType::Sqrt, Box::new(Expr::Int(4)))],
+                    qargs: Vec::new(),
+                },
+            ],
+        };
+
+        let mut visitor = ExpressionFolder::default();
+        visitor.visit_program(&mut program);
+
+        assert_eq!(program.statements.len(), 2);
+
+        // Check first call
+        if let Statement::GateCall {
+            name: _,
+            args,
+            qargs: _,
+        } = &program.statements[0]
+        {
+            assert_eq!(args.len(), 2);
+            assert_eq!(args[0], Expr::Float(1.5));
+            assert_eq!(args[1], Expr::Float(3.6 / -2.4));
+        } else {
+            panic!("Expected a gate call");
+        }
+
+        // Check second call
+        if let Statement::GateCall {
+            name: _,
+            args,
+            qargs: _,
+        } = &program.statements[1]
+        {
+            assert_eq!(args.len(), 1);
+            assert_eq!(args[0], Expr::Float(4f64.sqrt()));
         } else {
             panic!("Expected a gate call");
         }
