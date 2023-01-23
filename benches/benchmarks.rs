82c88018aa9c913974dab91e44d1f7b76bc177cc
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-01-23 16:13:09 +0100

    Remove useless benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -18,26 +18,10 @@ fn _setup() -> TensorNetwork {
 }
 
 #[bench]
-fn build_tensor(_b: &mut Bencher) {
+fn build_tensor(b: &mut Bencher) {
     let n = 5;
     let cycles = 5;
-    random_tensor_network(n, cycles);
+    b.iter(|| random_tensor_network(n, cycles));
 }
 
-#[bench]
-fn contract_tensor_(_b: &mut Bencher) {
-    let n = 5;
-    let cycles = 5;
-    let tn = random_tensor_network(n, cycles);
-    let mut bb = BranchBound::new(tn.clone(), None, 50, BranchBoundType::Flops);
-    bb.optimize_path(None);
-    let mut d_tn = Vec::new();
-    for r_t in tn.clone().get_tensors() {
-        d_tn.push(random_sparse_tensor(
-            r_t.clone(),
-            &tn.get_bond_dims(),
-            None,
-        ));
-    }
-    tn_contract(tn, d_tn, &vec![(0, 1), (0, 1)]);
-}
+// TODO: Implement benchmarking for contraction.
