1430e4e6da9c1e544a90625f073f17ee37697701
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-04-26 15:19:09 +0200

    Import tetra::Tensor as DataTensor

--- src/io.rs
+++ src/io.rs
@@ -5,9 +5,9 @@ use num_complex::Complex64;
 use std::collections::HashMap;
 use std::iter::zip;
 use tetra::Layout;
-use tetra::Tensor as TetraTensor;
+use tetra::Tensor as DataTensor;
 
-pub fn open_hdf5(file: &str) -> Result<(TensorNetwork, Vec<TetraTensor>)> {
+pub fn open_hdf5(file: &str) -> Result<(TensorNetwork, Vec<DataTensor>)> {
     let file = File::open(file)?;
     let gr = file.group("/tensors")?;
     let tensor_names = gr.member_names()?;
@@ -29,7 +29,7 @@ pub fn open_hdf5(file: &str) -> Result<(TensorNetwork, Vec<TetraTensor>)> {
         let tensor_dataset = gr.dataset(&tensor_name).unwrap().read_dyn::<Complex64>()?;
         let tensor_shape = tensor_dataset.shape().to_vec();
         t_tensors.push(Tensor::new(bond_ids.to_vec()));
-        let d_tensor = TetraTensor::new_from_flat(
+        let d_tensor = DataTensor::new_from_flat(
             &tensor_shape.iter().map(|&e| e as i32).collect::<Vec<i32>>(),
             tensor_dataset.into_raw_vec(),
             Some(Layout::RowMajor),
@@ -43,7 +43,7 @@ pub fn open_hdf5(file: &str) -> Result<(TensorNetwork, Vec<TetraTensor>)> {
 
     Ok((
         TensorNetwork::new(t_tensors, bond_dims, Some(&out_bond_ids.to_vec())),
-        d_tensors
+        d_tensors,
     ))
 }
 
@@ -53,8 +53,8 @@ mod tests {
     use crate::io::open_hdf5;
     use crate::tensornetwork::contraction::tn_contract;
     use itertools::Itertools;
-    use tetra::{Layout, Tensor as TetraTensor};
     use num_complex::Complex64;
+    use tetra::{Layout, Tensor as DataTensor};
 
     #[test]
     fn test_open_hdf5() {
@@ -64,11 +64,29 @@ mod tests {
         opt.optimize_path(None);
         let contract_path = opt.get_best_replace_path();
         let (r_tn, d_tn) = tn_contract(r_tn, d_tn, &contract_path);
-        let mut tn_sol = TetraTensor::new_from_flat(&[2, 2, 2, 2],
-            [0.7071067811865475, 0.7071067811865475, 0.0, 0.0,
-            0.0, 0.0, 0.7071067811865475, 0.7071067811865475,
-            0.0, 0.0, 0.7071067811865475, -0.7071067811865475, 
-            0.7071067811865475, -0.7071067811865475, 0.0, 0.0,].iter().map(|&e| Complex64::new(e, 0.0)).collect_vec(),
+        let mut tn_sol = DataTensor::new_from_flat(
+            &[2, 2, 2, 2],
+            [
+                0.7071067811865475,
+                0.7071067811865475,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.7071067811865475,
+                0.7071067811865475,
+                0.0,
+                0.0,
+                0.7071067811865475,
+                -0.7071067811865475,
+                0.7071067811865475,
+                -0.7071067811865475,
+                0.0,
+                0.0,
+            ]
+            .iter()
+            .map(|&e| Complex64::new(e, 0.0))
+            .collect_vec(),
             Some(Layout::RowMajor),
         );
 
