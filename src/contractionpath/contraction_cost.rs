19a5ccd2c9065aa2b491e520cebdcf1f6c402d22
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-07-27 14:13:33 +0200

    Update Tensor to allow bit-wise operations
    Allows useful helper functions to determine contractino results.
    Removes need to use HashSet to store leg ids.

--- src/contractionpath/contraction_cost.rs
+++ src/contractionpath/contraction_cost.rs
@@ -1,8 +1,9 @@
+use itertools::Itertools;
+
 use crate::tensornetwork::tensor::Tensor;
 use crate::tensornetwork::TensorNetwork;
-use std::collections::hash_map::RandomState;
-use std::collections::{HashMap, HashSet};
-use std::iter::FromIterator;
+use std::cmp::max;
+use std::collections::HashMap;
 
 /// Returns Schroedinger contraction time complexity of contracting [Tensor] objects at indices `i` and `j`.
 ///
@@ -25,8 +26,7 @@ use std::iter::FromIterator;
 /// ```
 pub fn contract_cost(tn: &TensorNetwork, i: usize, j: usize) -> u64 {
     // let kept_dims = tensor_a.symmetric_difference(&tensor_b);
-    _contract_cost(tn[i].clone(), tn[j].clone(), tn.get_bond_dims())
-    // shared_dims.into_iter().map(|e| tn.get_bond_dims()[e]).product()
+    _contract_cost(&tn[i], &tn[j], tn.get_bond_dims())
 }
 
 /// Returns Schroedinger contraction time complexity of contracting two [Tensor] objects.
@@ -42,21 +42,20 @@ pub fn contract_cost(tn: &TensorNetwork, i: usize, j: usize) -> u64 {
 /// ```
 /// # use tensorcontraction::tensornetwork::tensor::Tensor;
 /// # use tensorcontraction::tensornetwork::TensorNetwork;
-/// # use tensorcontraction::contractionpath::contraction_cost::contract_cost;
+/// # use tensorcontraction::contractionpath::contraction_cost::_contract_cost;
 /// let vec1 = Vec::from([0,1,2]);
 /// let vec2 = Vec::from([2,3,4]);
 /// let tn = TensorNetwork::from_vector(vec![Tensor::new(vec1), Tensor::new(vec2)], vec![5,7,9,11,13], None);
-/// assert_eq!(contract_cost(&tn, 0, 1), 45045);
+///
+/// assert_eq!(_contract_cost(&tn[0], &tn[1], tn.get_bond_dims()), 45045);
 /// ```
-pub fn _contract_cost(t_1: Tensor, t_2: Tensor, bond_dims: &HashMap<usize, u64>) -> u64 {
-    let tensor_a: HashSet<&usize, RandomState> = HashSet::from_iter(t_1.iter());
-    let tensor_b: HashSet<&usize, RandomState> = HashSet::from_iter(t_2.iter());
-
-    let shared_dims = tensor_a.union(&tensor_b);
-
-    // let kept_dims = tensor_a.symmetric_difference(&tensor_b);
-
-    shared_dims.into_iter().map(|e| bond_dims[*e]).product()
+pub fn _contract_cost(t_1: &Tensor, t_2: &Tensor, bond_dims: &HashMap<usize, u64>) -> u64 {
+    let shared_dims = t_1 | t_2;
+    shared_dims
+        .get_legs()
+        .iter()
+        .map(|e| bond_dims[e])
+        .product()
 }
 
 /// Returns Schroedinger contraction space complexity of contracting [Tensor] objects at indices `i` and `j` and the output
@@ -80,7 +79,7 @@ pub fn _contract_cost(t_1: Tensor, t_2: Tensor, bond_dims: &HashMap<usize, u64>)
 /// assert_eq!(contract_size(&tn, 0, 1), (Tensor::new(vec![0,1,3,4]), 6607));
 /// ```
 pub fn contract_size(tn: &TensorNetwork, i: usize, j: usize) -> (Tensor, u64) {
-    _contract_size(tn[i].clone(), tn[j].clone(), tn.get_bond_dims())
+    _contract_size(&tn[i], &tn[j], tn.get_bond_dims())
 }
 
 /// Returns Schroedinger contraction space complexity of contracting two [Tensor] objects
@@ -96,32 +95,48 @@ pub fn contract_size(tn: &TensorNetwork, i: usize, j: usize) -> (Tensor, u64) {
 /// ```
 /// # use tensorcontraction::tensornetwork::tensor::Tensor;
 /// # use tensorcontraction::tensornetwork::TensorNetwork;
-/// # use tensorcontraction::contractionpath::contraction_cost::contract_size;
+/// # use tensorcontraction::contractionpath::contraction_cost::_contract_size;
 /// let vec1 = Vec::from([0,1,2]);
 /// let vec2 = Vec::from([2,3,4]);
 /// let tn = TensorNetwork::from_vector(vec![Tensor::new(vec1), Tensor::new(vec2)], vec![5,7,9,11,13], None);
-/// assert_eq!(contract_size(&tn, 0, 1), (Tensor::new(vec![0,1,3,4]), 6607));
+/// assert_eq!(_contract_size(&tn[0], &tn[1], tn.get_bond_dims()), (Tensor::new(vec![0,1,3,4]), 6607));
 /// ```
-pub fn _contract_size(t_1: Tensor, t_2: Tensor, bond_dims: &HashMap<usize, u64>) -> (Tensor, u64) {
-    let legs_i = t_1.get_legs();
-    let legs_j = t_2.get_legs();
-    let mut diff = Vec::new();
-    for leg_i in legs_i.iter() {
-        if !legs_j.contains(leg_i) {
-            diff.push(*leg_i);
-        }
-    }
-
-    for leg_j in legs_j.iter() {
-        if !legs_i.contains(leg_j) {
-            diff.push(*leg_j);
-        }
-    }
+pub fn _contract_size(
+    t_1: &Tensor,
+    t_2: &Tensor,
+    bond_dims: &HashMap<usize, u64>,
+) -> (Tensor, u64) {
+    let diff = t_1 ^ t_2;
 
     let cost = diff.iter().map(|e| bond_dims[e]).product::<u64>()
-        + t_1.size(bond_dims)
-        + t_2.size(bond_dims);
-    (Tensor::new(diff), cost)
+        + t_1.iter().map(|e| bond_dims[e]).product::<u64>()
+        + t_2.iter().map(|e| bond_dims[e]).product::<u64>();
+
+    (Tensor::new(diff.iter().cloned().collect_vec()), cost)
+}
+
+/// Returns number of elements in a given [Tensor].
+///
+/// # Arguments
+///
+/// * `tensor` - Reference to [Tensor] object.
+/// * `bond_dims`- Dict of bond dimensions.
+///
+/// # Examples
+/// ```
+/// # use tensorcontraction::tensornetwork::tensor::Tensor;
+/// # use tensorcontraction::tensornetwork::TensorNetwork;
+/// # use tensorcontraction::contractionpath::contraction_cost::_tensor_size;
+/// let vec1 = Vec::from([0,1,2]);
+/// let tn = TensorNetwork::from_vector(vec![Tensor::new(vec1)], vec![5,7,9], None);
+/// assert_eq!(_tensor_size(&tn[0], tn.get_bond_dims()), 315);
+/// ```
+pub fn _tensor_size(tensor: &Tensor, bond_dims: &HashMap<usize, u64>) -> u64 {
+    tensor
+        .get_legs()
+        .iter()
+        .map(|e| bond_dims[e])
+        .product::<u64>()
 }
 
 /// Returns number of elements in a given [Tensor].
@@ -147,4 +162,41 @@ pub fn size(tn: &TensorNetwork, i: usize) -> u64 {
         .iter()
         .map(|e| tn.get_bond_dims()[e])
         .product::<u64>()
+}
+
+/// Returns Schroedinger contraction space complexity of contracting two [Tensor] objects
+///
+/// # Arguments
+///
+/// * `t_1` - First tensor to determine contraction cost.
+/// * `t_2`  - First tensor to determine contraction cost.
+/// * `bond_dims`- Dict of bond dimensions.
+///
+///
+/// # Examples
+/// ```
+/// # use tensorcontraction::tensornetwork::tensor::Tensor;
+/// # use tensorcontraction::tensornetwork::TensorNetwork;
+/// # use tensorcontraction::contractionpath::contraction_cost::contract_size;
+/// let vec1 = Vec::from([0,1,2]);
+/// let vec2 = Vec::from([2,3,4]);
+/// let tn = TensorNetwork::from_vector(vec![Tensor::new(vec1), Tensor::new(vec2)], vec![5,7,9,11,13], None);
+/// assert_eq!(contract_size(&tn, 0, 1), (Tensor::new(vec![0,1,3,4]), 6607));
+/// ```
+pub fn _contract_path_cost(
+    inputs: &[Tensor],
+    ssa_path: &[(usize, usize)],
+    bond_dims: &HashMap<usize, u64>,
+) -> (u64, u64) {
+    let mut op_cost = 0;
+    let mut mem_cost = 0;
+    let mut inputs = inputs.to_vec();
+    for (i, j) in ssa_path.iter().cloned() {
+        op_cost += _contract_cost(&inputs[i], &inputs[j], bond_dims);
+        let (k12, new_mem_cost) = _contract_size(&inputs[i], &inputs[j], bond_dims);
+        mem_cost = max(mem_cost, new_mem_cost);
+        inputs[i] = k12;
+    }
+
+    (op_cost, mem_cost)
 }
