e7980c0ea38023a82645ed8e4b7acfb385d49762
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-07-27 17:31:20 +0200

    Add mod candidates, contraction_cost, random_paths

--- src/contractionpath.rs
+++ src/contractionpath.rs
@@ -1,13 +1,62 @@
-use crate::tensornetwork::tensor::Tensor;
-
+use std::collections::HashMap;
+pub mod candidates;
 pub mod contraction_cost;
 pub mod paths;
+pub mod random_paths;
+// pub mod optimizer;
 
-// type alias to store contraction candidate when searching for optimal contraction path.
-struct Candidate {
-    flop_cost: u64,
-    size_cost: u64,
-    parent_ids: (usize, usize),
-    child_id: usize,
-    child_tensor: Tensor,
+/// The contraction ordering labels [Tensor] objects from each possible contraction with a
+/// unique identifier in ssa format. As only a subset of these [Tensor] objects are seen in
+/// a contraction path, the tensors in the optimal path search are not sequential. This converts
+/// the output to strictly obey an ssa format.
+///
+/// # Arguments
+///
+/// * `path` - Output path as Vec<(usize, usize)> after an [optimize_path] call.
+/// # Returns
+///
+/// Identical path using ssa format
+fn ssa_ordering(path: &Vec<(usize, usize, usize)>, mut n: usize) -> Vec<(usize, usize)> {
+    let mut ssa_path = Vec::with_capacity(path.len());
+    let mut hs = HashMap::new();
+    let path_len = n;
+    for (u1, u2, u3) in path {
+        let t1 = if *u1 > path_len { hs[u1] } else { *u1 };
+        let t2 = if *u2 > path_len { hs[u2] } else { *u2 };
+        hs.entry(*u3).or_insert(n);
+        n += 1;
+        ssa_path.push((t1, t2));
+    }
+    ssa_path
+}
+
+/// Accepts a contraction path that is in ssa format and returns a contraction path
+/// assuming that all contracted tensors replace the left input tensor.
+///
+/// # Arguments
+///
+/// * `path` - Output path as Vec<(usize, usize)> that is in ssa format.
+/// # Returns
+///
+/// Identical path that replaces the left input tensor upon contraction
+pub(super) fn ssa_replace_ordering(
+    path: &Vec<(usize, usize)>,
+    mut n: usize,
+) -> Vec<(usize, usize)> {
+    let mut ssa_path = Vec::with_capacity(path.len());
+    let mut hs = HashMap::new();
+    for tup in path.iter() {
+        // let mut tup = path[i];
+        let mut new_tup = *tup;
+        if hs.contains_key(&tup.0) {
+            new_tup.0 = hs[&tup.0];
+        }
+        if hs.contains_key(&tup.1) {
+            new_tup.1 = hs[&tup.1];
+        }
+        hs.entry(n).or_insert(new_tup.0);
+        ssa_path.push(new_tup);
+        n += 1;
+    }
+    ssa_path
 }
