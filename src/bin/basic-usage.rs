7cd7f0adc831913a23815b9c5d1d2d903ebd9abe
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-02 14:25:33 +0200

    Replace BranchBoundType
    Name changed to CostType to be more general.
    Some docstrings and basic-usage not updated.

--- src/bin/basic-usage.rs
+++ src/bin/basic-usage.rs
@@ -1,21 +1,18 @@
 extern crate tensorcontraction;
-use rand::rngs::StdRng;
-use rand::SeedableRng;
-use tensorcontraction::circuits::sycamore::{sycamore_circuit, sycamore_contract};
 use tensorcontraction::contractionpath::paths::{BranchBound, CostType, OptimizePath};
 use tensorcontraction::random::tensorgeneration::{random_sparse_tensor, random_tensor_network};
 // use tensorcontraction::tensornetwork::contraction::tn_contract;
 use tensorcontraction::tensornetwork::TensorNetwork;
 
 fn main() {
-    // let r_tn = random_tensor_network(4, 3);
-    // let mut d_tn = Vec::new();
-    // for r_t in r_tn.get_tensors() {
-    //     d_tn.push(random_sparse_tensor(r_t, r_tn.get_bond_dims(), None));
-    // }
-    // let mut opt = BranchBound::new(&r_tn, None, 20, CostType::Flops);
-    // opt.optimize_path();
-    // let opt_path = opt.get_best_replace_path();
+    let r_tn = random_tensor_network(4, 3);
+    let mut d_tn = Vec::new();
+    for r_t in r_tn.get_tensors() {
+        d_tn.push(random_sparse_tensor(r_t, r_tn.get_bond_dims(), None));
+    }
+    let mut opt = BranchBound::new(&r_tn, None, 20, CostType::Flops);
+    opt.optimize_path();
+    let opt_path = opt.get_best_replace_path();
 
     // tn_contract(r_tn, d_tn, &opt_path);
 
