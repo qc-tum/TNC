9c491a8451f5e9ece878efb2e5420284eff184a1
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-02 14:25:33 +0200

    Replace BranchBoundType
    Name changed to CostType to be more general.
    Some docstrings and basic-usage not updated.

--- src/bin/basic-usage.rs
+++ src/bin/basic-usage.rs
@@ -1,5 +1,5 @@
 extern crate tensorcontraction;
-use tensorcontraction::contractionpath::paths::{BranchBound, BranchBoundType, OptimizePath};
+use tensorcontraction::contractionpath::paths::{BranchBound, CostType, OptimizePath};
 use tensorcontraction::random::tensorgeneration::{random_sparse_tensor, random_tensor_network};
 use tensorcontraction::tensornetwork::contraction::tn_contract;
 
@@ -9,8 +9,8 @@ fn main() {
     for r_t in r_tn.get_tensors() {
         d_tn.push(random_sparse_tensor(r_t, r_tn.get_bond_dims(), None));
     }
-    let mut opt = BranchBound::new(&r_tn, None, 20, BranchBoundType::Flops);
-    opt.optimize_path(None);
+    let mut opt = BranchBound::new(&r_tn, None, 20, CostType::Flops);
+    opt.optimize_path();
     let opt_path = opt.get_best_replace_path();
 
     tn_contract(r_tn, d_tn, &opt_path);
