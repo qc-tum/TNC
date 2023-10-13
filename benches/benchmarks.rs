cfa8c92f4c8791f420f2ef640ff57d981ac5625e
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-13 13:46:20 +0200

    Uncomment criterion benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -24,31 +24,31 @@ fn sized_contraction(r_tn: TensorNetwork, d_tn: Vec<tetra::Tensor>) {
     tn_contract(r_tn, d_tn, &vec![(0, 1)]);
 }
 
-// pub fn criterion_benchmark(c: &mut Criterion) {
-//     let mut rng = StdRng::seed_from_u64(52);
-//     let mut mul_group = c.benchmark_group("Multiplication");
-//     mul_group.measurement_time(Duration::from_secs(10));
-//     mul_group.sampling_mode(SamplingMode::Flat);
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = StdRng::seed_from_u64(52);
+    let mut mul_group = c.benchmark_group("Multiplication");
+    mul_group.measurement_time(Duration::from_secs(10));
+    mul_group.sampling_mode(SamplingMode::Flat);
 
-//     for k in [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384] {
-//         let n = 64;
-//         let t1 = Tensor::new(vec![0, 1]);
-//         let t2 = Tensor::new(vec![2, 1, 3, 4]);
-//         let r_tn = TensorNetwork::from_vector(vec![t1, t2], vec![n, k, n, n, n], None);
+    for k in [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384] {
+        let n = 64;
+        let t1 = Tensor::new(vec![0, 1]);
+        let t2 = Tensor::new(vec![2, 1, 3, 4]);
+        let r_tn = TensorNetwork::from_vector(vec![t1, t2], vec![n, k, n, n, n], None);
 
-//         let d_tn: Vec<tetra::Tensor> = r_tn
-//             .get_tensors()
-//             .iter()
-//             .map(|tensor| {
-//                 random_sparse_tensor_with_rng(tensor, r_tn.get_bond_dims(), None, &mut rng)
-//             })
-//             .collect();
-//         mul_group.bench_function(BenchmarkId::from_parameter(k), |b| {
-//             b.iter(|| sized_contraction(r_tn.clone(), d_tn.clone()));
-//         });
-//     }
-//     mul_group.finish();
-// }
+        let d_tn: Vec<tetra::Tensor> = r_tn
+            .get_tensors()
+            .iter()
+            .map(|tensor| {
+                random_sparse_tensor_with_rng(tensor, r_tn.get_bond_dims(), None, &mut rng)
+            })
+            .collect();
+        mul_group.bench_function(BenchmarkId::from_parameter(k), |b| {
+            b.iter(|| sized_contraction(r_tn.clone(), d_tn.clone()));
+        });
+    }
+    mul_group.finish();
+}
 
 pub fn partition_benchmark(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(52);
