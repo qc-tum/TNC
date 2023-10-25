e4e476c3c872aeda4d022b2bae37c5eca3a57d3e
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-25 13:19:41 +0200

    Update benchmark to match updated tensorgeneration

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,27 +1,10 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode};
-use rand::{rngs::StdRng, Rng, SeedableRng};
+use rand::{rngs::StdRng, SeedableRng};
 use std::time::Duration;
 use tensorcontraction::{
-    random::tensorgeneration::random_sparse_tensor_with_rng,
-    tensornetwork::{contraction::tn_contract, tensor::Tensor, TensorNetwork},
+    random::tensorgeneration::create_filled_tensor_network,
+    tensornetwork::{contraction::contract_tensor_network, tensor::Tensor},
 };
-
-#[allow(dead_code)]
-fn tetra_contraction<R>(r_tn: TensorNetwork, opt_path: &Vec<(usize, usize)>, rng: &mut R)
-where
-    R: Rng + ?Sized,
-{
-    let d_tn = r_tn
-        .get_tensors()
-        .iter()
-        .map(|tensor| random_sparse_tensor_with_rng(tensor, r_tn.get_bond_dims(), None, rng))
-        .collect();
-    tn_contract(r_tn, d_tn, opt_path);
-}
-
-fn sized_contraction(r_tn: TensorNetwork, d_tn: Vec<tetra::Tensor>) {
-    tn_contract(r_tn, d_tn, &vec![(0, 1)]);
-}
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(52);
@@ -33,17 +16,11 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let n = 64;
         let t1 = Tensor::new(vec![0, 1]);
         let t2 = Tensor::new(vec![2, 1, 3, 4]);
-        let r_tn = TensorNetwork::from_vector(vec![t1, t2], vec![n, k, n, n, n], None);
+        let bond_dims = [(0, n), (1, k), (2, n), (3, n), (4, n)];
+        let r_tn = create_filled_tensor_network(vec![t1, t2], &bond_dims.into(), None, &mut rng);
 
-        let d_tn: Vec<tetra::Tensor> = r_tn
-            .get_tensors()
-            .iter()
-            .map(|tensor| {
-                random_sparse_tensor_with_rng(tensor, r_tn.get_bond_dims(), None, &mut rng)
-            })
-            .collect();
         mul_group.bench_function(BenchmarkId::from_parameter(k), |b| {
-            b.iter(|| sized_contraction(r_tn.clone(), d_tn.clone()));
+            b.iter(|| contract_tensor_network(&mut r_tn.clone(), &vec![(0, 1)]));
         });
     }
     mul_group.finish();
