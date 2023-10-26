cd58c195ef36e8d71b5fbb356ba4949d25282091
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-26 03:21:41 +0200

    Update partitioning to use new Tensor API
    Shift to heirarchical structure.
    Use new Vertex type.

--- src/tensornetwork/partitioning.rs
+++ src/tensornetwork/partitioning.rs
@@ -1,16 +1,15 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::iter::zip;
+
+use super::tensor::Tensor;
+use crate::types::Vertex;
+
 use kahypar_sys;
 use kahypar_sys::{partition, KaHyParContext};
 
-use super::TensorNetwork;
-
-pub fn partition_tn(
-    partitioning: &mut Vec<i32>,
-    tn: &mut TensorNetwork,
-    k: i32,
-    config_file: String,
-) {
+pub fn find_partitioning(tn: &mut Tensor, k: i32, config_file: String) -> Vec<usize> {
     let num_vertices = tn.get_tensors().len() as u32;
-    assert!(partitioning.len() == num_vertices as usize);
     let num_hyperedges = tn.get_edges().len() as u32;
     let mut context = KaHyParContext::new();
     context.configure(config_file);
@@ -21,18 +20,21 @@ pub fn partition_tn(
     let mut hyperedge_indices = vec![0];
     let mut hyperedges = vec![];
     let bond_dims = tn.get_bond_dims();
-    for (edges, tensor_ids) in tn.get_edges().clone() {
+    for (edges, tensor_ids) in tn.get_edges() {
         hyperedge_weights.push(bond_dims[&edges] as i32);
         let mut length = 0;
         for id in tensor_ids {
-            if id.is_some() {
-                hyperedges.push(id.unwrap() as u32);
-                length += 1;
+            match id {
+                Vertex::Closed(id) => {
+                    hyperedges.push(*id as u32);
+                    length += 1;
+                }
+                Vertex::Open => continue,
             }
         }
         hyperedge_indices.push(hyperedge_indices.last().unwrap() + length);
     }
-
+    let mut partitioning = vec![-1; num_vertices as usize];
     partition(
         num_vertices,
         num_hyperedges,
@@ -44,9 +46,36 @@ pub fn partition_tn(
         hyperedges.as_slice(),
         &mut objective,
         &mut context,
-        partitioning,
+        &mut partitioning,
     );
-    tn.partitioning = partitioning.to_vec();
+    partitioning
+        .iter()
+        .map(|e| *e as usize)
+        .collect::<Vec<usize>>()
+}
+
+pub fn partition_tensor_network(tn: &Tensor, partitioning: &[usize]) -> Tensor {
+    let partition_ids = partitioning
+        .iter()
+        .cloned()
+        .unique()
+        .collect::<Vec<usize>>();
+    let partition_dict = HashMap::<usize, usize>::from_iter(zip(
+        partition_ids.iter().cloned(),
+        0..partition_ids.len(),
+    ));
+    let mut partitions = vec![Tensor::default(); partition_ids.len()];
+
+    for (partition_id, tensor) in zip(partitioning.iter(), tn.tensors.iter()) {
+        partitions[partition_dict[partition_id]].push_tensor(
+            tensor.clone(),
+            Some(&tensor.get_bond_dims()),
+            None,
+        );
+    }
+    let mut partitioned_tn = Tensor::default();
+    partitioned_tn.push_tensors(partitions, Some(&*tn.get_bond_dims()), None);
+    partitioned_tn
 }
 
 #[cfg(test)]
@@ -54,17 +83,17 @@ mod tests {
     use std::env;
     use std::path::PathBuf;
 
+    use crate::tensornetwork::create_tensor_network;
     use crate::tensornetwork::tensor::Tensor;
-    use crate::tensornetwork::TensorNetwork;
 
-    use super::partition_tn;
+    use super::{find_partitioning, partition_tensor_network};
 
     fn get_current_working_dir() -> std::io::Result<PathBuf> {
         env::current_dir()
     }
 
-    fn setup_complex() -> TensorNetwork {
-        TensorNetwork::from_vector(
+    fn setup_complex() -> Tensor {
+        create_tensor_network(
             vec![
                 Tensor::new(vec![4, 3, 2]),
                 Tensor::new(vec![0, 1, 3, 2]),
@@ -73,7 +102,21 @@ mod tests {
                 Tensor::new(vec![10, 8, 9]),
                 Tensor::new(vec![5, 1, 0]),
             ],
-            vec![27, 18, 12, 15, 5, 3, 18, 22, 45, 65, 5, 17],
+            &[
+                (0, 27),
+                (1, 18),
+                (2, 12),
+                (3, 15),
+                (4, 5),
+                (5, 3),
+                (6, 18),
+                (7, 22),
+                (8, 45),
+                (9, 65),
+                (10, 5),
+                (11, 17),
+            ]
+            .into(),
             None,
         )
     }
@@ -81,13 +124,31 @@ mod tests {
     #[test]
     fn test_simple_partitioning() {
         let mut tn = setup_complex();
-        let mut partitioning: Vec<i32> = vec![-1; tn.get_tensors().len() as usize];
-        partition_tn(
-            &mut partitioning,
-            &mut tn,
-            2,
-            std::string::String::from("test/km1"),
+        let mut ref_tensor_1 = Tensor::default();
+        let mut ref_tensor_2 = Tensor::default();
+        let mut ref_tensor_3 = Tensor::default();
+        ref_tensor_1.push_tensors(
+            vec![Tensor::new(vec![6, 8, 9]), Tensor::new(vec![10, 8, 9])],
+            Some(&tn.get_bond_dims()),
+            None,
         );
-        assert_eq!(tn.partitioning, [1, 1, 0, 0, 0, 1]);
+        ref_tensor_2.push_tensors(
+            vec![Tensor::new(vec![0, 1, 3, 2]), Tensor::new(vec![5, 1, 0])],
+            Some(&tn.get_bond_dims()),
+            None,
+        );
+        ref_tensor_3.push_tensors(
+            vec![Tensor::new(vec![4, 3, 2]), Tensor::new(vec![4, 5, 6])],
+            Some(&tn.get_bond_dims()),
+            None,
+        );
+        let partitioning = find_partitioning(&mut tn, 3, std::string::String::from("tests/km1"));
+        assert_eq!(partitioning, [2, 1, 2, 0, 0, 1]);
+        let partitioned_tn = partition_tensor_network(&tn, partitioning.as_slice());
+        assert_eq!(partitioned_tn.get_tensors().len(), 3);
+
+        assert_eq!(partitioned_tn.get_tensors()[0], ref_tensor_3);
+        assert_eq!(partitioned_tn.get_tensors()[1], ref_tensor_2);
+        assert_eq!(partitioned_tn.get_tensors()[2], ref_tensor_1);
     }
 }
