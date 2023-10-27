88659cfa044d0d60a8afb7ad9dfc62b744a61072
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2023-10-27 23:25:09 +0200

    Correct benchmark examples

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,18 +1,14 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode};
 use rand::{rngs::StdRng, SeedableRng};
 use std::time::Duration;
+use tensorcontraction::circuits::sycamore::sycamore_circuit;
+use tensorcontraction::contractionpath::paths::{CostType, Greedy, OptimizePath};
+use tensorcontraction::tensornetwork::partitioning::{find_partitioning, partition_tensor_network};
+use tensorcontraction::types::ContractionIndex;
 use tensorcontraction::{
-    circuits::sycamore::{sycamore_circuit, sycamore_contract},
-    contractionpath::paths::{CostType, Greedy, OptimizePath},
+    path,
     random::tensorgeneration::create_filled_tensor_network,
-    random::tensorgeneration::random_sparse_tensor_with_rng,
     tensornetwork::{contraction::contract_tensor_network, tensor::Tensor},
-    tensornetwork::{
-        contraction::{tn_contract, tn_contract_partition},
-        partitioning::partition_tn,
-        tensor::Tensor,
-        TensorNetwork,
-    },
 };
 
 pub fn criterion_benchmark(c: &mut Criterion) {
@@ -29,7 +25,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let r_tn = create_filled_tensor_network(vec![t1, t2], &bond_dims.into(), None, &mut rng);
 
         mul_group.bench_function(BenchmarkId::from_parameter(k), |b| {
-            b.iter(|| contract_tensor_network(&mut r_tn.clone(), &vec![(0, 1)]));
+            b.iter(|| contract_tensor_network(&mut r_tn.clone(), &[path![0, 1]]));
         });
     }
     mul_group.finish();
@@ -41,48 +37,25 @@ pub fn partition_benchmark(c: &mut Criterion) {
     mul_group.measurement_time(Duration::from_secs(10));
     mul_group.sampling_mode(SamplingMode::Flat);
 
-    for k in [5, 10, 15, 20] {
-        let mut r_tn = sycamore_circuit(k, 10, None, None, &mut rng);
+    for k in [15, 20] {
+        let mut r_tn = sycamore_circuit(k, 5, None, None, &mut rng);
+        let partitioning = find_partitioning(&mut r_tn, 5, std::string::String::from("tests/km1"));
+        let partitioned_tn = partition_tensor_network(&r_tn, &partitioning);
+        // let mut opt = BranchBound::new(&r_tn, None, 20, CostType::Flops);
+        let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
 
-        let d_tn: Vec<tetra::Tensor> = r_tn
-            .get_tensors()
-            .iter()
-            .map(|tensor| {
-                random_sparse_tensor_with_rng(tensor, r_tn.get_bond_dims(), None, &mut rng)
-            })
-            .collect();
-
-        let mut partitioning: Vec<i32> = vec![-1; r_tn.get_tensors().len() as usize];
-        partition_tn(
-            &mut partitioning,
-            &mut r_tn,
-            k as i32,
-            std::string::String::from("test/km1"),
-        );
-        let mut opt = Greedy::new(&r_tn, CostType::Flops);
-        let mut opt_paths = vec![];
-        for i in 0..5 {
-            opt.optimize_partitioned_path(i);
-            opt_paths.push(opt.get_best_partition_replace_path(i as usize));
-        }
+        opt.optimize_path();
+        let path = opt.get_best_replace_path();
 
         mul_group.bench_function(BenchmarkId::from_parameter(k), |b| {
             b.iter(|| {
-                let mut local_r_tn = r_tn.clone();
-                let mut local_d_tn = d_tn.clone();
-                for i in 0..5 {
-                    tn_contract_partition(
-                        &mut local_r_tn,
-                        &mut local_d_tn,
-                        i as i32,
-                        &opt_paths[i],
-                    );
-                }
+                let mut local_r_tn = partitioned_tn.clone();
+                contract_tensor_network(&mut local_r_tn, &path);
             });
         });
     }
     mul_group.finish();
 }
 
-criterion_group!(benches, partition_benchmark, criterion_benchmark);
+criterion_group!(benches, partition_benchmark, criterion_benchmark,);
 criterion_main!(benches);
