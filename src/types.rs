63ed17164181265cbbb3279e07ee31a5507a7243
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2024-02-16 13:38:06 +0100

    Remove unused import

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,5 @@
 use std::collections::hash_map::DefaultHasher;
-use std::{
-    convert::From,
-    hash::{Hash, Hasher},
-};
+use std::hash::{Hash, Hasher};
 // use std::hash::DefaultHasher;
 
 use memoffset::offset_of;
