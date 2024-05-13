0e7c42b90dc643a2b85b471741d9b85172e9c57c
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2024-05-13 19:07:12 +0200

    Add Hadamard gate

--- src/gates.rs
+++ src/gates.rs
@@ -22,6 +22,13 @@ pub fn load_gate(gate: &str, angles: Option<&Vec<f64>>) -> DataTensor {
         Complex64::new(-1.0, 0.0),
     ];
 
+    const H: [Complex64; 4] = [
+        Complex64::new(0.5, 0.0),
+        Complex64::new(0.5, 0.0),
+        Complex64::new(0.5, 0.0),
+        Complex64::new(-0.5, 0.0),
+    ];
+
     const SQRX: [Complex64; 4] = [
         Complex64::new(0.5, 0.5),
         Complex64::new(0.5, -0.5),
@@ -65,6 +72,7 @@ pub fn load_gate(gate: &str, angles: Option<&Vec<f64>>) -> DataTensor {
         "X" => DataTensor::new_from_flat(&[2, 2], X.to_vec(), None),
         "Y" => DataTensor::new_from_flat(&[2, 2], Y.to_vec(), None),
         "Z" => DataTensor::new_from_flat(&[2, 2], Z.to_vec(), None),
+        "H" => DataTensor::new_from_flat(&[2, 2], H.to_vec(), None),
         "CX" => DataTensor::new_from_flat(&[2, 2, 2, 2], CX.to_vec(), None),
         "SQRX" => DataTensor::new_from_flat(&[2, 2], SQRX.to_vec(), None),
         "SQRY" => DataTensor::new_from_flat(&[2, 2], SQRY.to_vec(), None),
@@ -76,24 +84,24 @@ pub fn load_gate(gate: &str, angles: Option<&Vec<f64>>) -> DataTensor {
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
-                Complex64::new(1.0, 0.0),
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
-                Complex64::new(1.0, 0.0),
                 Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
-                Complex64::new(1.0, 0.0),
+                Complex64::new(0.0, 0.0),
+                Complex64::new(0.0, 0.0),
+                Complex64::new(0.0, 0.0),
                 Complex64::new(0.0, 0.0),
             ];
             let angles = angles.unwrap();
             fsim[5] = Complex64::new(angles[0].cos(), 0.0);
-            fsim[10] = Complex64::new(angles[0].cos(), 0.0);
-            fsim[6] = Complex64::new(0.0, angles[0].sin());
-            fsim[11] = Complex64::new(0.0, angles[0].sin());
-            fsim[15] = Complex64::new(0.0, angles[1].exp());
+            fsim[6] = Complex64::new(0.0, -angles[0].sin());
+            fsim[9] = Complex64::new(angles[0].cos(), 0.0);
+            fsim[10] = Complex64::new(0.0, -angles[0].sin());
+            fsim[15] = Complex64::new(0.0, (-angles[1]).exp());
             DataTensor::new_from_flat(&[2, 2, 2, 2], fsim.to_vec(), None)
         }
         _ => todo!(), // _ => capture_gate(&gate),
