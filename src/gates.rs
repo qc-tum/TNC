ae27dd903bd14e268ea028366bc44787787f61c2
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2024-05-14 15:21:40 +0200

    Correct gates

--- src/gates.rs
+++ src/gates.rs
@@ -1,6 +1,8 @@
 use num_complex::Complex64;
 use tetra::Tensor as DataTensor;
 
+use std::f64::consts::FRAC_1_SQRT_2;
+
 pub fn load_gate(gate: &str, angles: Option<&Vec<f64>>) -> DataTensor {
     const X: [Complex64; 4] = [
         Complex64::new(0.0, 0.0),
@@ -23,10 +25,10 @@ pub fn load_gate(gate: &str, angles: Option<&Vec<f64>>) -> DataTensor {
     ];
 
     const H: [Complex64; 4] = [
-        Complex64::new(0.5, 0.0),
-        Complex64::new(0.5, 0.0),
-        Complex64::new(0.5, 0.0),
-        Complex64::new(-0.5, 0.0),
+        Complex64::new(FRAC_1_SQRT_2, 0.0),
+        Complex64::new(FRAC_1_SQRT_2, 0.0),
+        Complex64::new(FRAC_1_SQRT_2, 0.0),
+        Complex64::new(-FRAC_1_SQRT_2, 0.0),
     ];
 
     const SQRX: [Complex64; 4] = [
@@ -99,8 +101,8 @@ pub fn load_gate(gate: &str, angles: Option<&Vec<f64>>) -> DataTensor {
             let angles = angles.unwrap();
             fsim[5] = Complex64::new(angles[0].cos(), 0.0);
             fsim[6] = Complex64::new(0.0, -angles[0].sin());
-            fsim[9] = Complex64::new(angles[0].cos(), 0.0);
-            fsim[10] = Complex64::new(0.0, -angles[0].sin());
+            fsim[10] = Complex64::new(angles[0].cos(), 0.0);
+            fsim[9] = Complex64::new(0.0, -angles[0].sin());
             fsim[15] = Complex64::new(0.0, (-angles[1]).exp());
             DataTensor::new_from_flat(&[2, 2, 2, 2], fsim.to_vec(), None)
         }
