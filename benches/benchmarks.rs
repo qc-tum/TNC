a15c3c142ccd9259057198155ee52bd3274ac306
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2024-06-17 15:46:19 +0200

    Change benchmark functions for more accurate timings

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,8 +1,7 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
 use mpi::topology::SimpleCommunicator;
 use mpi::traits::{Communicator, CommunicatorCollectives, Root};
 use rand::{rngs::StdRng, SeedableRng};
-use std::time::Duration;
 use tensorcontraction::contractionpath::paths::OptimizePath;
 use tensorcontraction::contractionpath::paths::{greedy::Greedy, CostType};
 use tensorcontraction::mpi::communication::{
@@ -23,10 +22,8 @@ use tensorcontraction::{
 pub fn multiplication_benchmark(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(52);
     let mut mul_group = c.benchmark_group("Multiplication");
-    mul_group.measurement_time(Duration::from_secs(10));
-    mul_group.sampling_mode(SamplingMode::Flat);
 
-    for k in [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384] {
+    for k in [16, 32, 64] {
         let n = 64;
         let t1 = Tensor::new(vec![0, 1]);
         let t2 = Tensor::new(vec![2, 1, 3, 4]);
@@ -34,7 +31,11 @@ pub fn multiplication_benchmark(c: &mut Criterion) {
         let r_tn = create_filled_tensor_network(vec![t1, t2], &bond_dims.into(), None, &mut rng);
 
         mul_group.bench_function(BenchmarkId::from_parameter(k), |b| {
-            b.iter(|| contract_tensor_network(&mut r_tn.clone(), &[path![0, 1]]));
+            b.iter_batched_ref(
+                || r_tn.clone(),
+                |tn| contract_tensor_network(tn, black_box(&[path![0, 1]])),
+                BatchSize::SmallInput,
+            );
         });
     }
     mul_group.finish();
@@ -44,23 +45,23 @@ pub fn multiplication_benchmark(c: &mut Criterion) {
 pub fn partitioned_contraction_benchmark(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(23);
     let mut part_group = c.benchmark_group("Partition");
-    part_group.measurement_time(Duration::from_secs(10));
-    part_group.sampling_mode(SamplingMode::Flat);
 
     for k in [10, 15, 20, 25] {
         let r_tn = sycamore_circuit(k, 5, 0.4, 0.4, &mut rng, ConnectivityLayout::Osprey);
         let partitioning =
             find_partitioning(&r_tn, 5, String::from("tests/km1_kKaHyPar_sea20.ini"), true);
         let partitioned_tn = partition_tensor_network(&r_tn, &partitioning);
+
         let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
-
         opt.optimize_path();
-
         let path = opt.get_best_replace_path();
+
         part_group.bench_function(BenchmarkId::from_parameter(k), |b| {
-            b.iter(|| {
-                contract_tensor_network(&mut partitioned_tn.clone(), &path.clone());
-            });
+            b.iter_batched_ref(
+                || partitioned_tn.clone(),
+                |tn| contract_tensor_network(tn, &path),
+                BatchSize::SmallInput,
+            );
         });
     }
     part_group.finish();
@@ -72,19 +73,15 @@ pub fn parallel_naive_benchmark(c: &mut Criterion) {
     let mut rng: StdRng = StdRng::seed_from_u64(51);
 
     let mut par_part_group = c.benchmark_group("MPI Naive");
-    par_part_group.measurement_time(Duration::from_secs(30));
-    par_part_group.sampling_mode(SamplingMode::Flat);
 
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
     let size = world.size();
     let rank = world.rank();
 
-    // Do we need to know communication beforehand?
+    // TODO: Do we need to know communication beforehand?
     for k in [25, 30] {
-        let mut partitioned_tn = Tensor::default();
-        let mut path = Vec::new();
-        if rank == 0 {
+        let (partitioned_tn, path) = if rank == 0 {
             let r_tn = sycamore_circuit(k, 20, 0.4, 0.4, &mut rng, ConnectivityLayout::Osprey);
             let partitioning = find_partitioning(
                 &r_tn,
@@ -92,12 +89,15 @@ pub fn parallel_naive_benchmark(c: &mut Criterion) {
                 String::from("tests/km1_kKaHyPar_sea20.ini"),
                 true,
             );
-            partitioned_tn = partition_tensor_network(&r_tn, &partitioning);
-            let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
+            let partitioned_tn = partition_tensor_network(&r_tn, &partitioning);
 
+            let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
             opt.optimize_path();
-            path = opt.get_best_replace_path();
-        }
+            let path = opt.get_best_replace_path();
+            (partitioned_tn, path)
+        } else {
+            Default::default()
+        };
         world.barrier();
 
         par_part_group.bench_function(BenchmarkId::from_parameter(k), |b| {
@@ -106,7 +106,8 @@ pub fn parallel_naive_benchmark(c: &mut Criterion) {
                     scatter_tensor_network(&partitioned_tn, &path, rank, size, &world);
                 contract_tensor_network(&mut local_tn, &local_path);
 
-                naive_reduce_tensor_network(&mut local_tn.clone(), &path, rank, size, &world);
+                naive_reduce_tensor_network(&mut local_tn, &path, rank, size, &world);
+                local_tn
             });
         });
     }
@@ -143,19 +144,15 @@ pub fn parallel_partition_benchmark(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(52);
 
     let mut par_part_group = c.benchmark_group("MPI Partition");
-    par_part_group.measurement_time(Duration::from_secs(10));
-    par_part_group.sampling_mode(SamplingMode::Flat);
 
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
     let size = world.size();
     let rank = world.rank();
 
-    // Do we need to know communication beforehand?
+    // TODO: Do we need to know communication beforehand?
     for k in [30, 35, 60, 80] {
-        let mut partitioned_tn = Tensor::default();
-        let mut path = Vec::new();
-        if rank == 0 {
+        let (partitioned_tn, path) = if rank == 0 {
             let r_tn = sycamore_circuit(k, 20, 0.4, 0.4, &mut rng, ConnectivityLayout::Osprey);
             let partitioning = find_partitioning(
                 &r_tn,
@@ -163,12 +160,15 @@ pub fn parallel_partition_benchmark(c: &mut Criterion) {
                 String::from("tests/km1_kKaHyPar_sea20.ini"),
                 true,
             );
-            partitioned_tn = partition_tensor_network(&r_tn, &partitioning);
-            let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
+            let partitioned_tn = partition_tensor_network(&r_tn, &partitioning);
 
+            let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
             opt.optimize_path();
-            path = opt.get_best_replace_path();
-        }
+            let path = opt.get_best_replace_path();
+            (partitioned_tn, path)
+        } else {
+            Default::default()
+        };
         world.barrier();
 
         par_part_group.bench_function(BenchmarkId::from_parameter(k), |b| {
@@ -184,6 +184,7 @@ pub fn parallel_partition_benchmark(c: &mut Criterion) {
                 };
                 world.barrier();
                 intermediate_reduce_tensor_network(&mut local_tn, &path, rank, size, &world);
+                local_tn
             });
         });
     }
