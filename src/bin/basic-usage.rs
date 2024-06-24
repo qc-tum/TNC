c2a5d2a922b1defe31d8623a5ae8c4c5bd5f83c8
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2024-06-24 11:26:00 +0200

    Change basic-usage back to contraction

--- src/bin/basic-usage.rs
+++ src/bin/basic-usage.rs
@@ -1,57 +1,76 @@
-extern crate tensorcontraction;
-
+use mpi::traits::{Communicator, CommunicatorCollectives};
 use rand::rngs::StdRng;
 use rand::SeedableRng;
-
-use tensorcontraction::contractionpath::contraction_cost::contract_cost_tensors;
-use tensorcontraction::contractionpath::contraction_tree::balance_partitions_iter;
-use tensorcontraction::contractionpath::paths::greedy::Greedy;
-use tensorcontraction::contractionpath::paths::{CostType, OptimizePath};
+use tensorcontraction::contractionpath::paths::{greedy::Greedy, CostType, OptimizePath};
+use tensorcontraction::mpi::communication::{
+    broadcast_path, intermediate_reduce_tensor_network, scatter_tensor_network,
+};
 use tensorcontraction::networks::connectivity::ConnectivityLayout;
 use tensorcontraction::networks::sycamore::random_circuit;
+use tensorcontraction::tensornetwork::contraction::contract_tensor_network;
 use tensorcontraction::tensornetwork::partitioning::{find_partitioning, partition_tensor_network};
 
-use std::time::{Duration, Instant};
-
+// Run with at least 2 processes
 fn main() {
-    let mut rng: StdRng = StdRng::seed_from_u64(27);
+    let mut rng = StdRng::seed_from_u64(23);
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let size = world.size();
+    let rank = world.rank();
+    let root = world.process_at_rank(0);
 
-    let num_qubits = 20;
-    let circuit_depth = 10;
-    let single_qubit_probability = 0.4;
-    let two_qubit_probability = 0.4;
-    let connectivity = ConnectivityLayout::Osprey;
-    let num_partitions = 4;
+    if rank == 0 {
+        println!("Running with {size} processes");
+    }
 
-    let tensor = random_circuit(
-        num_qubits,
-        circuit_depth,
-        single_qubit_probability,
-        two_qubit_probability,
-        &mut rng,
-        connectivity,
-    );
-    // Find vec of partitions
-    let partitioning = find_partitioning(
-        &tensor,
-        num_partitions,
-        String::from("tests/km1_kKaHyPar_sea20.ini"),
-        true,
-    );
+    let k = 20;
 
-    let partitioned_tn = partition_tensor_network(&tensor, &partitioning);
-    let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
-    opt.optimize_path();
-    let path = opt.get_best_replace_path();
-    let now = Instant::now();
-    let (cost, new_tree, contraction_path, costs) = balance_partitions_iter(
-        &partitioned_tn,
-        &path,
-        false,
-        1,
-        120,
-        String::from("output/rebalance_trial"),
-        contract_cost_tensors,
-    );
-    println!("{:?}", now.duration_since(now));
+    // Setup tensor network
+    // TODO: Do we need to know communication beforehand?
+    let (mut partitioned_tn, path) = if rank == 0 {
+        let r_tn = random_circuit(k, 10, 0.4, 0.4, &mut rng, ConnectivityLayout::Osprey);
+        let partitioned_tn = if size > 1 {
+            let partitioning = find_partitioning(
+                &r_tn,
+                size,
+                String::from("tests/km1_kKaHyPar_sea20.ini"),
+                true,
+            );
+            partition_tensor_network(&r_tn, &partitioning)
+        } else {
+            r_tn
+        };
+        let mut opt = Greedy::new(&partitioned_tn, CostType::Flops);
+
+        opt.optimize_path();
+        let path = opt.get_best_replace_path();
+        (partitioned_tn, path)
+    } else {
+        Default::default()
+    };
+    world.barrier();
+
+    // Distribute tensor network and contract
+    let local_tn = if size > 1 {
+        let (mut local_tn, local_path) =
+            scatter_tensor_network(&partitioned_tn, &path, rank, size, &world);
+        contract_tensor_network(&mut local_tn, &local_path);
+
+        let path = if rank == 0 {
+            broadcast_path(&path[(size as usize)..path.len()], &root, &world)
+        } else {
+            broadcast_path(&[], &root, &world)
+        };
+        world.barrier();
+        intermediate_reduce_tensor_network(&mut local_tn, &path, rank, size, &world);
+        local_tn
+    } else {
+        contract_tensor_network(&mut partitioned_tn, &path);
+        partitioned_tn
+    };
+
+    // Print the result
+    if rank == 0 {
+        println!("{local_tn:?}");
+    }
 }
