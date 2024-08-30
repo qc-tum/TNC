a6efb9d37e068702475d9489f4f73bf372c04fda
Keefe Huang <30915130+keefehuang@users.noreply.github.com>
2024-08-30 12:59:33 +0200

    Rename output of parse_logs to be cleaner

--- examples/parse_logs.rs
+++ examples/parse_logs.rs
@@ -3,11 +3,13 @@ use tensorcontraction::contractionpath::contraction_tree::import::logs_to_pdf;
 fn main() {
     let filename = "balanced_usage";
     let suffix = "opt.log.json";
-    let ranks = 4;
-    logs_to_pdf(filename, suffix, ranks, "opt");
+    let ranks = 2;
+
+    logs_to_pdf(filename, suffix, ranks, "contraction_optimized");
 
     let filename = "balanced_usage";
     let suffix = "unopt.log.json";
-    let ranks = 4;
-    logs_to_pdf(filename, suffix, ranks, "unopt");
+    let ranks = 2;
+
+    logs_to_pdf(filename, suffix, ranks, "contraction_unoptimized");
 }
