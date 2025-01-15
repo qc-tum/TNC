3cd010a125d2b38a65f88fd5433bc23755db40c6
Manuel Geiger <40306539+Ectras@users.noreply.github.com>
2025-01-15 16:24:57 +0100

    Make RankTensorMapping check existing entries

--- src/mpi/mpi_types.rs
+++ src/mpi/mpi_types.rs
@@ -3,7 +3,10 @@ use serde::{Deserialize, Serialize};
 
 use crate::types::TensorIndex;
 
-/// A bidirectional mapping between MPI ranks and composite tensors.
+/// A bidirectional (1:1) mapping between MPI ranks and composite tensors.
+///
+/// Each valid tensor index should map to a rank, but not every rank has to map to a
+/// tensor.
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct RankTensorMapping(Vec<(Rank, TensorIndex)>);
 
@@ -12,20 +15,27 @@ impl RankTensorMapping {
         Self(Vec::with_capacity(capacity))
     }
 
+    /// Inserts a new mapping between an MPI rank and a tensor.
     pub fn insert(&mut self, rank: Rank, tensor: TensorIndex) {
+        assert_eq!(self.tensor(rank), None);
+        assert_eq!(self.rank_opt(tensor), None);
         self.0.push((rank, tensor));
     }
 
-    /// Gets the MPI rank associated with the given tensor.
-    pub fn rank(&self, tensor: TensorIndex) -> Rank {
-        self.0
-            .iter()
-            .find(|(_, t)| *t == tensor)
-            .map(|(r, _)| *r)
-            .unwrap()
+    /// Gets the MPI rank associated with the given tensor (if any).
+    #[inline]
+    fn rank_opt(&self, tensor: TensorIndex) -> Option<Rank> {
+        self.0.iter().find(|(_, t)| *t == tensor).map(|(r, _)| *r)
     }
 
-    /// Gets the tensor associated with the given MPI rank (if any).
+    /// Gets the MPI rank associated with the given tensor. Each tensor should have
+    /// a unique rank associated with it.
+    pub fn rank(&self, tensor: TensorIndex) -> Rank {
+        self.rank_opt(tensor).unwrap()
+    }
+
+    /// Gets the tensor associated with the given MPI rank (if any). As there can be
+    /// more ranks than tensors, not every rank has to be associated with a tensor.
     pub fn tensor(&self, rank: Rank) -> Option<TensorIndex> {
         self.0.iter().find(|(r, _)| *r == rank).map(|(_, t)| *t)
     }
